7d9155c6ce67fb2a851d2e500e188ac7b6621689
Luca <lucaairoldi92@gmail.com>
2022-11-11 14:46:18 +0100

    Update

--- 2021/day09/9b.go
+++ 2021/day09/9b.go
@@ -31,104 +31,26 @@ func main() {
 			matrice[appoggio] = risultato{int(in[j] - '0'), false}
 		}
 	}
-	
+
 	//stampa(matrice)
 
 	for i := 0; i < lunghezza; i++ {
 		for j := 0; j < lunghezza; j++ {
-			if i == 0 {
-				if j == 0 {
-					if matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val && matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				} else if j == lunghezza-1 {
-					if matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				} else {
-					if matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val && matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				}
-				//controllo lato dx
-			} else if i == lunghezza-1 {
-				if j == 0 {
-					if matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val && matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				} else if j == lunghezza-1 {
-					if matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val && matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				} else {
-					if matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val && matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val {
-						x = area(matrice, i, j)
-						c += matrice[punto{i, j}].val
-						if x != 0 {
-							finale = append(finale, x)
-						}
-					}
-				}
-				//controllo lato top
-			} else if j == 0 {
-				if matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val {
-					x = area(matrice, i, j)
-					c += matrice[punto{i, j}].val
-					if x != 0 {
-						finale = append(finale, x)
-					}
-				}
-				//controllo lato bot
-			} else if j == lunghezza-1 {
-				if matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val {
-					x = area(matrice, i, j)
-					c += matrice[punto{i, j}].val
-					if x != 0 {
-						finale = append(finale, x)
-					}
-				}
-				//caso generale
-			} else {
-				if matrice[punto{i, j}].val < matrice[punto{i - 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i + 1, j}].val && matrice[punto{i, j}].val < matrice[punto{i, j + 1}].val && matrice[punto{i, j}].val < matrice[punto{i, j - 1}].val {
-					x = area(matrice, i, j)
-					c += matrice[punto{i, j}].val
-					if x != 0 {
-						finale = append(finale, x)
-					}
-				}
-			}
+			
 		}
 	}
 	sort.Ints(finale)
+	fmt.Println(finale)
 	fmt.Println("B:", finale[len(finale)-1]*finale[len(finale)-2]*finale[len(finale)-3])
 }
 
-func area(matrice map[punto]risultato, x, y int) int {
+func area(matrice map[punto]risultato, x, y, c int) int {
 	if matrice[punto{x, y}].flag == true || matrice[punto{x, y}].val == 9 || matrice[punto{x, y}].val == 0 {
-		return 0
+		return c
 	}
+	c++
 	matrice[punto{x, y}] = risultato{matrice[punto{x, y}].val, true}
-	return 1 + area(matrice, x+1, y) + area(matrice, x, y+1) + area(matrice, x-1, y) + area(matrice, x, y-1)
+	return area(matrice, x+1, y, c) + area(matrice, x, y+1, c) + area(matrice, x-1, y, c) + area(matrice, x, y-1, c)
 }
 
 func stampa(matrice map[punto]risultato) {
