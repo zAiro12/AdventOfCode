9439ad7096107b00163829bfcd01db655884e0ed
Luca <lucaairoldi92@gmail.com>
2022-11-12 09:49:51 +0100

    ADD day 16 (non finito)

--- 2021/day16/16.go
+++ 2021/day16/16.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
 	var dec string
 	fmt.Scan(&dec)
@@ -11,19 +18,17 @@ func main() {
 	for scanner.Scan() {
 		in := scanner.Text()
 		fmt.Sscanf(in, "%d = %s", &uno, &due)
-		if uno == "" {
-			continue
-		}
 		operazioni[uno] = due
 	}
 
-	decToBin(dec, operazioni)
+	fmt.Println(decToBin(dec, operazioni))
 }
 
 func decToBin(s string, operazioni map[int]string) int {
 	var out string
 	for i := 0; i < len(s); i++ {
-		out += operazioni[s[i]]
+		out += operazioni[int(s[i])]
 	}
-	
+	bin, _ := strconv.Atoi(out)
+	return bin
 }
