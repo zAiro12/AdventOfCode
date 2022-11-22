7cc2690b43977269b3ca93813ce36cf8b87daff7
Luca <lucaairoldi92@gmail.com>
2022-11-22 15:16:46 +0100

    upload

--- 2021/day18/18.go
+++ 2021/day18/18.go
@@ -9,14 +9,14 @@ import (
 func main() {
 	albero := ReadTree("[[[1,2],3]")
 	fmt.Println(PrintTree(albero.Root))
-	fmt.Println(zairo.Max(3, 5))
+	fmt.Println(zairo.Max(1, 2))
 }
 
-func ReadTree(s string) zairo.Albero {
-	return zairo.Albero{nil}
+func ReadTree(s string) Albero {
+	return Albero{nil}
 }
 
-func PrintTree(nodo *zairo.NodoAlbero) string {
+func PrintTree(nodo *NodoAlbero) string {
 	var s string
 
 	return s
