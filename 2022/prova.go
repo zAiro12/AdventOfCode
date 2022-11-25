6c9c351573fdad7c2020c9008597256f158a8dfc
Luca <lucaairoldi92@gmail.com>
2022-11-25 18:48:44 +0100

    fix

--- 2022/prova.go
+++ 2022/prova.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	zairo "github.com/zAiro12/AdventOfCode/utile"
 )
 
 func main() {
 	g := zairo.NuovoGrafo(8)
 	zairo.AggiungiCoppieGrafo(g)
-
+	zairo.StampaGrafo(g)
 }
