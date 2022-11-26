2c41d6774e78b6a65068c04594d786e4df61b9d0
Luca <lucaairoldi92@gmail.com>
2022-11-26 10:51:02 +0100

    update

--- 2022/prova.go
+++ 2022/prova.go
@@ -6,15 +6,6 @@ import (
 	zairo "github.com/zAiro12/AdventOfCode/utile"
 )
 
-type nodopila struct {
-	val  int
-	next *nodopila
-}
-type Grafo struct {
-	Len       int
-	Adiacenti []*nodopila
-}
-
 func main() {
 	g := zairo.NuovoGrafo(8)
 	zairo.AggiungiCoppieGrafo(g)
