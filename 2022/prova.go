aa8b71166d8d2256e7bfc537448ba7cacefe2ce2
Luca <lucaairoldi92@gmail.com>
2022-11-26 11:36:27 +0100

    add script

--- 2022/prova.go
+++ 2022/prova.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	zairo "github.com/zAiro12/AdventOfCode/utile"
 )
 
@@ -10,17 +8,4 @@ func main() {
 	g := zairo.NuovoGrafo(8)
 	zairo.AggiungiCoppieGrafo(g)
 	zairo.StampaGrafo(g)
-
-func Trova(grafo *zairo.Grafo, x, y int) bool {
-	if x > grafo.Len || x < 0 {
-		return false
-	}
-	appoggio := grafo.Adiacenti[x]
-	for appoggio != nil {
-		if appoggio.Val == y {
-			return true
-		}
-		appoggio = appoggio.Next
-	}
-	return false
 }
---
+++ script.sh
@@ -0,0 +1,19 @@
+SHELL=/bin/bash
+
+giorno=$(date +'%d')
+anno=$(date +'%Y')
+echo "$giorno $anno"
+cd /
+cd /Users/zairo/Library/Mobile\ Documents/com~apple~CloudDocs/AdventOfCode/AdventOfCode/$anno
+mkdir day$giorno
+cd day$giorno
+
+touch prova.txt
+
+curl https://adventofcode.com/$anno/day/$giorno/input -o input.txt -H "cookie: session=53616c7465645f5f42ead91e71c09e0e007085c54e5123b2f618ee4da95986febdcf8b23a8edaaefd112dd540a55954bc400a14c8f652acdb6a2c834717fed7d"
+
+echo 'package main ' > $giorno.go
+echo '' >> $giorno.go
+echo 'func main(){' >> $giorno.go
+echo '' >> $giorno.go
+echo '}' >> $giorno.go
