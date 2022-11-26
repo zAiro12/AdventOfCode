25f4c235316b40bfe35d87a94194bc4b081333be
Luca <lucaairoldi92@gmail.com>
2022-11-26 10:09:24 +0100

    update utile/grafi

--- 2022/prova.go
+++ 2022/prova.go
@@ -1,11 +1,11 @@
 package main
 
-import ("github.com/")
+import (
+	zairo "github.com/zAiro12/AdventOfCode/utile"
+)
 
 func main() {
-	g := NuovoGrafo(8)
-	AggiungiCoppieGrafo(g)
-	StampaGrafo(g)
+	g := zairo.NuovoGrafo(8)
+	zairo.AggiungiCoppieGrafo(g)
+	zairo.StampaGrafo(g)
 }
-
-
