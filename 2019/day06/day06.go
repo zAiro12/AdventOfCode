23ead75867f4de0b6a28e08ffcf3cefcf3560cca
Luca <lucaairoldi92@gmail.com>
2022-11-30 15:12:05 +0100

    update

--- 2019/day06/day06.go
+++ 2019/day06/day06.go
@@ -7,26 +7,32 @@ import (
 
 func main() {
 	var in string
-	mappa := make(map[string]string)
+	padri := make(map[string]string)
+	
 	for {
 		_, err := fmt.Scan(&in)
 		if err != nil {
 			break
 		}
 		trim := strings.Split(in, ")")
-		mappa[trim[1]] = trim[0]
+		padri[trim[1]] = trim[0]
 	}
 
-	Stampa(mappa)
-
-	var c int
-	for k := range mappa {
-		c += conta(k, mappa)
-	}
-	fmt.Println(c)
+	fmt.Println(risolviA(padri))
+	fmt.Println(risolviB(padri))
 
 }
 
+//risolvi parte a usando funzione conta
+func risolviA(padri map[string]string) int {
+	var c int
+	for k := range padri {
+		c += conta(k, padri)
+	}
+	return c
+}
+
+//parte a
 func conta(s string, m map[string]string) int {
 	if len(m[s]) != 0 {
 		return 1 + conta(m[s], m)
@@ -35,7 +41,29 @@ func conta(s string, m map[string]string) int {
 	}
 }
 
-func Stampa(mappa map[string]string) {
+func risolviB(padri map[string]string) int {
+	var passi int
+	visitatiSan := make(map[string]int)
+	visitatiYou := make(map[string]int)
+	san := "SAN"
+	you := "YOU"
+
+	for i := 0; san != "COM" || you != "COM"; i++ {
+		san = padri[san]
+		you = padri[you]
+		fmt.Println("debug:", san, you)
+
+		visitatiSan[san] = i
+		visitatiYou[you] = i
+
+	}
+	fmt.Println(visitatiSan)
+	fmt.Println(visitatiYou)
+	return passi
+}
+
+
+func stampa(mappa map[string]string) {
 	for k, v := range mappa {
 		fmt.Println(k, ")", v)
 	}
