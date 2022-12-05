e4f25670db18489ad1032d573224a43d447ac6da
Luca <lucaairoldi92@gmail.com>
2022-12-05 14:15:18 +0100

    add 2022/day05

--- 2022/day05/05.go
+++ 2022/day05/05.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"unicode"
 
 	zairo "github.com/zAiro12/AdventOfCode/utile"
 )
@@ -16,35 +16,41 @@ func main() {
 
 	file := zairo.Input(os.Args[1])
 	sc := bufio.NewScanner(file)
-	for i := 0; i < 10; i++ {
-		sc.Scan()
 
-		in = sc.Text()
-		split := strings.Split(in, "")
-		if i == 9 {
-			break
-		}
-		stack1[i+1] = append(stack1[i+1], split...)
-		stack2[i+1] = append(stack2[i+1], split...)
-	}
-
-	var quanti, da, a int
 	for sc.Scan() {
-		fmt.Sscanf(sc.Text(), "move %d from %d to %d", &quanti, &da, &a)
+		in = sc.Text()
+		nPile := (len(in) + 1) / 4
+		var quanti, da, a int
 
-		stack1 = parteA(quanti, da, a, stack1)
-		stack2 = parteB(quanti, da, a, stack2)
+		if in != "" && in[0] != 'm' {
+			index := 1
+			for j := 1; j <= nPile; j++ {
+				val := in[(index)]
+				zairo.Log("val", string(val))
+				if unicode.IsLetter(rune(val)) {
+					stack1[j] = append(stack1[j], string(val))
+					stack2[j] = append(stack2[j], string(val))
+				}
+				index += 4
+			}
+
+		} else if in != "" && in[0] == 'm' {
+			fmt.Sscanf(sc.Text(), "move %d from %d to %d", &quanti, &da, &a)
+
+			zairo.Log(stack1)
+			stack1 = parteA(quanti, da, a, stack1)
+			stack2 = parteB(quanti, da, a, stack2)
+
+		}
 	}
 
 	var risultatoA, risultatoB string
 	for i := 1; i <= len(stack1); i++ {
 		risultatoA += stack1[i][0]
 	}
-    for i := 1; i <= len(stack2); i++ {
+	for i := 1; i <= len(stack2); i++ {
 		risultatoB += stack2[i][0]
 	}
-
-
 	zairo.StampaA(risultatoA)
 	zairo.StampaB(risultatoB)
 }
