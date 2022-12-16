c84719a448ba395eeae6c4e3024f5b8ff4dd6e63
Luca <lucaairoldi92@gmail.com>
2022-12-16 09:20:10 +0100

    add

--- 2022/day13/13.go
+++ 2022/day13/13.go
@@ -1,12 +1,48 @@
-package main 
+package main
 
 import (
-    zairo "github.com/zAiro12/AdventOfCode/utile"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	zairo "github.com/zAiro12/AdventOfCode/utile"
 )
 
-func main(){
+func main() {
 
+	sc := bufio.NewScanner(zairo.Input(os.Args[1]))
 
-    zairo.StampaA()
-    zairo.StampaB()
+	var in string
+	for sc.Scan() {
+		in = sc.Text()
+		riga1 := riempiRiga(in)
+		stampaSlice(riga1)
+
+		sc.Scan()
+		in = sc.Text()
+		riga2 := riempiRiga(in)
+		stampaSlice(riga2)
+
+		sc.Scan()
+	}
+
+	zairo.StampaA()
+	zairo.StampaB()
+}
+
+func riempiRiga(s string) (input []any) {
+	s = s[1 : len(s)-1]
+	split := strings.Split(s, ",")
+	for i := 0; i < len(split); i++ {
+		input = append(input, split[i])
+	}
+	return
+}
+
+func stampaSlice(slice []any) {
+	for i := 0; i < len(slice); i++ {
+		zairo.Log("slice", i, slice[i])
+	}
+	fmt.Println()
 }
