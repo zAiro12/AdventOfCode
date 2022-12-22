0bee808ebe6a7774b581c327767c2bbbcfe8eb31
Luca <lucaairoldi92@gmail.com>
2022-12-22 10:31:20 +0100

    Update 15.go

--- 2022/day15/15.go
+++ 2022/day15/15.go
@@ -20,7 +20,7 @@ type dato struct {
 
 func main() {
 
-	insieme := dato{make(map[zairo.Point]bool), make(map[zairo.Point]zairo.Point), 0, 0, 0, 0}
+	insieme := dato{make(map[zairo.Point]bool), make(map[zairo.Point]zairo.Point), 0, 20, 0, 20}
 
 	sc := bufio.NewScanner(zairo.Input(os.Args[1]))
 	var sensore, beacon zairo.Point
@@ -36,7 +36,7 @@ func main() {
 	zairo.Log("min/max", insieme.minX, insieme.maxX, insieme.minY, insieme.maxY)
 	zairo.StampaA(insieme.parteA())
 	//insieme.stampaMatrice()
-	zairo.StampaB()
+	zairo.StampaB(insieme.parteB())
 }
 
 func (insieme *dato) aggiornaMatrice(sensore, beacon zairo.Point) {
@@ -45,24 +45,6 @@ func (insieme *dato) aggiornaMatrice(sensore, beacon zairo.Point) {
 	insieme.matrice[beacon] = true
 	insieme.legami[sensore] = beacon
 
-	minX := zairo.Min(sensore.X, beacon.X)
-	maxX := zairo.Max(sensore.X, beacon.X)
-	minY := zairo.Min(sensore.Y, beacon.Y)
-	maxY := zairo.Max(sensore.Y, beacon.Y)
-
-	if minX < insieme.minX {
-		insieme.minX = minX
-	}
-	if maxX > insieme.maxX {
-		insieme.maxX = maxX
-	}
-
-	if minY < insieme.minY {
-		insieme.minY = minY
-	}
-	if maxY > insieme.maxY {
-		insieme.maxY = maxY
-	}
 }
 
 func (insieme dato) stampaMatrice() {
@@ -78,26 +60,12 @@ func (insieme dato) stampaMatrice() {
 	}
 }
 
-func (insieme dato) stampaRiga(y int) {
-	var c int
-	for i := insieme.minX; i <= insieme.maxX; i++ {
-
-		if insieme.matrice[zairo.Point{X: i, Y: y}] {
-			fmt.Print("#")
-			c++
-		} else {
-			fmt.Print(".")
-		}
-	}
-	fmt.Println(c)
-}
-
 func (insieme *dato) parteA() int {
 	var distanza int
 	for sensor, beacon := range insieme.legami {
-		zairo.Log("ciclo", sensor, beacon)
 		distanza = calcolaDistanza(sensor, beacon)
-		insieme.creaMacchia(sensor, distanza)
+
+		insieme.creaRiga(sensor, distanza)
 	}
 	var counter int
 	for i := insieme.minX; i <= insieme.maxX; i++ {
@@ -113,7 +81,6 @@ func calcolaDistanza(puntoA, puntoB zairo.Point) int {
 }
 
 func (insieme *dato) creaMacchia(start zairo.Point, distanza int) {
-	zairo.Log("distanza", distanza)
 	var tmp1, tmp2, tmp3, tmp4 zairo.Point
 	for i := 0; i <= distanza; i++ {
 		for j := 0; j <= distanza; j++ {
@@ -124,36 +91,64 @@ func (insieme *dato) creaMacchia(start zairo.Point, distanza int) {
 
 			if calcolaDistanza(tmp1, start) <= distanza {
 				insieme.matrice[tmp1] = true
-				if tmp1.X > insieme.maxX {
-					insieme.maxX = tmp1.X
-				} else if tmp1.Y > insieme.maxY {
-					insieme.maxY = tmp1.Y
-				}
+
 			}
 			if calcolaDistanza(tmp2, start) <= distanza {
 				insieme.matrice[tmp2] = true
-				if tmp2.X > insieme.maxX {
-					insieme.maxX = tmp2.X
-				} else if tmp2.Y < insieme.minY {
-					insieme.minY = tmp2.Y
-				}
+
 			}
 			if calcolaDistanza(tmp3, start) <= distanza {
 				insieme.matrice[tmp3] = true
-				if tmp3.X < insieme.minX {
-					insieme.minX = tmp3.X
-				} else if tmp3.Y < insieme.minX {
-					insieme.minY = tmp3.Y
-				}
+
 			}
 			if calcolaDistanza(tmp4, start) <= distanza {
 				insieme.matrice[tmp4] = true
-				if tmp4.X < insieme.minX {
-					insieme.minX = tmp4.X
-				} else if tmp4.Y > insieme.maxY {
-					insieme.maxY = tmp4.Y
-				}
+
 			}
 		}
 	}
+}
+
+func (insieme *dato) creaRiga(start zairo.Point, distanza int) {
+	r := zairo.ABS(2000000 - start.Y)
+
+	if distanza >= r {
+
+		for i := start.X - (distanza - r); i <= start.X+(distanza-r); i++ {
+			insieme.matrice[zairo.Point{X: i, Y: 2000000}] = true
+		}
+
+		if start.X-(distanza-r) < insieme.minX {
+			insieme.minX = start.X - (distanza - r)
+		}
+
+		if start.X+(distanza-r) > insieme.maxX {
+			insieme.maxX = start.X + (distanza - r)
+		}
+	}
+
+}
+
+func (insieme *dato) parteB() int {
+	insieme.minX, insieme.minY = 0, 0
+	//insieme.maxX, insieme.maxY = 20, 20
+	insieme.maxX, insieme.maxY = 4000000, 4000000
+
+	var distanza int
+	for sensor, beacon := range insieme.legami {
+		distanza = calcolaDistanza(sensor, beacon)
+
+		// insieme.creaMacchia(sensor, distanza)
+		
+	}
+
+	for y := insieme.minY; y <= insieme.maxY; y++ {
+		for x := insieme.minX; x <= insieme.maxX; x++ {
+			if insieme.matrice[zairo.Point{X: x, Y: y}] {
+				return x*4000000 + y
+			}
+		}
+	}
+
+	return 0
 }
