00c42c0344e368c657cfd6a124c758131c32cee8
Luca Airoldi <lucaairoldi92@gmail.com>
2023-12-03 10:46:05 +0100

    2023 03

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ I make [**Advent of Code**](http://adventofcode.com/) in Go and Python. This rep
 
 ## Risultati
 
-### [2023](https://github.com/zAiro12/AdventOfCode/tree/main/2023) (2⭐)
+### [2023](https://github.com/zAiro12/AdventOfCode/tree/main/2023) (6⭐)
 
 |         | Parte 1 | Parte 2 |
 | :-----: | :-----: | :-----: |
 | [Giorno 1](https://adventofcode.com/2023/day/1)     | ⭐ | ⭐ |
 | [Giorno 2](https://adventofcode.com/2023/day/2)     | ⭐ | ⭐ |
-<!-- 
 | [Giorno 3](https://adventofcode.com/2023/day/3)     | ⭐ | ⭐ |
+<!-- 
 | [Giorno 4](https://adventofcode.com/2023/day/4)     | ⭐ | ⭐ |
 | [Giorno 5](https://adventofcode.com/2023/day/5)     | ⭐ | ⭐ |
 | [Giorno 6](https://adventofcode.com/2023/day/6)     | ⭐ | ⭐ |
--- aocScript.sh
+++ aocScript.sh
@@ -2,24 +2,15 @@ SHELL=/bin/bash
 
 giorno=$(date +'%d')
 anno=$(date +'%Y')
-echo "$giorno $anno"
 cd $anno
 mkdir day$giorno
 cd day$giorno
 
 touch prova.txt
 
-curl https://adventofcode.com/$anno/day/$giorno/input -o input.txt -H "cookie: session=53616c7465645f5f42ead91e71c09e0e007085c54e5123b2f618ee4da95986febdcf8b23a8edaaefd112dd540a55954bc400a14c8f652acdb6a2c834717fed7d"
+giorno=$(date +'%e' | xargs)
+echo $giorno $anno
+curl https://adventofcode.com/$anno/day/$giorno/input -o input.txt -H "cookie: session=53616c7465645f5f3bac7e5afb0c288b901110d6bddc8184363945e1d7bbee41eb4ef3dc2bfebdc119880970ed2854121838ee942abbb75abfcd96d184fbc6ad"
 
-echo 'package main ' > $giorno.go
-echo '' >> $giorno.go
-echo 'import (' >> $giorno.go
-echo '    zairo "github.com/zAiro12/AdventOfCode/utile"'>> $giorno.go
-echo ')' >> $giorno.go
-echo '' >> $giorno.go
-echo 'func main(){' >> $giorno.go
-echo '' >> $giorno.go
-echo '' >> $giorno.go
-echo '    zairo.StampaA()' >> $giorno.go
-echo '    zairo.StampaB()' >> $giorno.go
-echo '}' >> $giorno.go
+touch a.py
+touch b.py
