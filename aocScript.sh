ddfb437045ec1e584544b2b98d33bf12f8286014
Luca Airoldi <lucaairoldi92@gmail.com>
2023-12-06 11:14:19 +0100

    modifica script per fare advent of code in c#

--- aocScript.sh
+++ aocScript.sh
@@ -12,5 +12,4 @@ giorno=$(date +'%e' | xargs)
 echo $giorno $anno
 curl https://adventofcode.com/$anno/day/$giorno/input -o input.txt -H "cookie: session=53616c7465645f5f3bac7e5afb0c288b901110d6bddc8184363945e1d7bbee41eb4ef3dc2bfebdc119880970ed2854121838ee942abbb75abfcd96d184fbc6ad"
 
-touch a.py
-touch b.py
+dotnet new console
