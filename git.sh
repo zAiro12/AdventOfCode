e653a49dd0188217cf7db04dfafc69753131b75e
Luca Airoldi <lucaairoldi92@gmail.com>
2023-12-13 11:37:36 +0100

    remove .csproj

--- git.sh
+++
@@ -1,9 +0,0 @@
-SHELL=/bin/bash
-
-giorno=$(date +'%d')
-anno=$(date +'%Y')
-
-cd /Users/zairo/Library/Mobile\ Documents/com~apple~CloudDocs/AdventOfCode/AdventOfCode
-git add *
-git commit -m"$anno $giorno"
-git push
