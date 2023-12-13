0a0194ed80e3ffb4de8cc440a8ada8ff7c5e26e4
Luca Airoldi <lucaairoldi92@gmail.com>
2023-12-13 11:33:01 +0100

    change

---
+++ 2023/day07/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program{
+
+    public enum Valori{
+        
+        
+        
+    }
+
+
+    private static void Main(string[] args){
+        Console.WriteLine($"parte a: {A()}");
+        Console.WriteLine($"parte a: {B()}");
+    }
+
+    public static int A(){
+        
+
+
+        int totale = 0;
+
+        return totale;
+    }
+    public static int B(){
+
+
+
+        int totale = 0;
+
+        return totale;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code
 <p align="center">
-    <img src="https://user-images.githubusercontent.com/16360374/49324718-7954f100-f4e8-11e8-8ef6-1b701afc504f.png" width="225"/>
+    <img src="https://user-images.githubusercontent.com/16360374/49324718-7954f100-f4e8-11e8-8ef6-1b701afc504f.png" width="225" alt=""/>
 </p>  
 
 <a href="http://www.wtfpl.net/"><img
