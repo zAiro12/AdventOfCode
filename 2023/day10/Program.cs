1815ea2034083e20acb7aae4676bc54e4e157d0b
Luca Airoldi <lucaairoldi92@gmail.com>
2024-08-21 10:50:10 +0200

    aggiunti giorni anno 2023

---
+++ 2023/day10/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Parte A: {A(args[0])}");
+        // Console.WriteLine($"Parte B: {B(args[0])}");
+    }
+
+    public static int A(string input){
+        StreamReader sr = new(input);
+
+        string? rawLine = sr.ReadLine();
+
+        
+
+        int totale = 0;
+        return totale;
+    }
+}
