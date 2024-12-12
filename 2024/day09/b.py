caa33002fb678eb913074be46adc9c353ebe4e94
Luca <lucaairoldi92@gmail.com>
2024-12-12 07:42:31 +0100

    inizio impl 9b day 11 script

--- 2024/day09/b.py
+++ 2024/day09/b.py
@@ -1,2 +1,27 @@
-file = open('input.txt')
-# file = open('2024/day09/prova.txt')
+with open('prova.txt', 'r') as file:
+    disk_map = file.readline().strip()
+
+array = []
+liberi = {}
+idNum = 0
+isNum = True
+for char in disk_map:
+    if isNum:
+        array.append(str(idNum) * int(char))
+        isNum = False
+        idNum += 1
+    else:
+        if char != '0':
+            array.append(-1 * int(char))
+            liberi[len(array)-1] = int(char)
+        isNum = True
+
+sx, dx = 0, len(array) - 1
+while sx < dx:
+    while sx < len(array) and type(array[sx]) == str:
+        sx += 1
+    while dx >= 0 and type(array[sx]) != str:
+        dx -= 1
+    print(sx, dx)
+    # if sx < dx:
+    #     array[sx], array[dx] = array[dx], array[sx]
---
+++ day11/a.py
@@ -0,0 +1,2 @@
+file = open('input.txt') 
+# file = open('/day11/prova.txt') 
