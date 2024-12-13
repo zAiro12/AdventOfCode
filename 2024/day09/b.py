ec5fe105f94d02782ff216fba31088c6e6d34955
Luca <lucaairoldi92@gmail.com>
2024-12-13 13:16:17 +0100

    implementazione 9b non va

--- 2024/day09/b.py
+++ 2024/day09/b.py
@@ -1,4 +1,5 @@
-with open('prova.txt', 'r') as file:
+with open('input.txt', 'r') as file:
+# with open('2024/day09/prova.txt', 'r') as file:
     disk_map = file.readline().strip()
 
 array = []
@@ -15,18 +16,37 @@ for char in disk_map:
         isNum = True
 
 sx, dx = 0, len(array) - 1
-print(array)
+
 while sx < dx:
-    while sx < len(array) and type(array[sx]) == str:
-        sx += 1
-    while dx >= 0 and type(array[sx]) != str:
+    while dx >= 0 and type(array[dx]) != str:
         dx -= 1
-    print(sx, dx)
-    if sx < dx and abs(array[sx]) >= len(array[dx]):
+    while sx < len(array):
+        if type(array[sx]) is int and abs(array[sx]) >= len(array[dx]):
+            break
+        sx += 1
+    # dx -= 1
+
+    if sx < dx:
         array[sx] = array[sx] + len(array[dx])
         if array[sx] == 0:
             array.pop(sx)
+            dx -= 1
         array.insert(sx, array[dx])
-        array[dx] = 0
+        array[dx + 1] = -1 * len(array[sx])
+        
+    sx, dx = 0, dx - 1
+    sx = 0
 
-print(array)
+result = ""
+for item in array:
+    if isinstance(item, str):
+        result += item
+    elif isinstance(item, int):
+        result += '.' * abs(item)
+
+checksum = 0
+for i, char in enumerate(result):
+    if char != '.':
+        checksum += int(char) * (i)
+
+print("Checksum:", checksum)
